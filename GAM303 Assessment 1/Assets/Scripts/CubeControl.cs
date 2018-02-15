3478eb560fa26b2b31cc813ce380169e4e3d6f31
NooraD <ndaoud@myjmc.edu.au>
2018-02-15 16:00:58 +1100

    Testing

---
+++ GAM303 Assessment 1/Assets/Scripts/CubeControl.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeControl : MonoBehaviour {
+	public float moveSpeed;
+
+	// Use this for initialization
+	void Start () 
+	{
+		moveSpeed	= 1f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			transform.Translate (Vector3.left * Time.deltaTime * moveSpeed);
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			transform.Translate (Vector3.right * Time.deltaTime * moveSpeed);
+		}
+
+		if (Input.GetKey (KeyCode.UpArrow)) 
+		{
+			transform.Translate (Vector3.up * Time.deltaTime * moveSpeed);
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) 
+		{
+			transform.Translate (Vector3.down * Time.deltaTime * moveSpeed);
+		}
+	}
+}
