0fe91618d556b67c57c98ea98d69a9a09666f4f9
HannibalMontannibal <33308759+HannibalMontannibal@users.noreply.github.com>
2018-02-19 13:04:54 +1100

    Progress
    Working more on A1

--- GAM303 Assessment 1/Assets/Scripts/Apple.cs
+++ GAM303 Assessment 1/Assets/Scripts/Apple.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Apple : MonoBehaviour {
+public class Apple : MonoBehaviour 
+{
+	public static float bottomY = -20f;
 
 	// Use this for initialization
 	void Start () {
@@ -10,7 +12,12 @@ public class Apple : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		//removes apples that hit -20f units
+		if (transform.position.y < bottomY)
+		{
+			Destroy (this.gameObject);
+		}	
 	}
 }
--- GAM303 Assessment 1/Assets/Scripts/AppleTree.cs
+++ GAM303 Assessment 1/Assets/Scripts/AppleTree.cs
@@ -2,15 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AppleTree : MonoBehaviour {
+public class AppleTree : MonoBehaviour 
+{
+	// prefab for instantiating apples.
+	public GameObject applePrefab;
+
+	// speed at which the AppleTree moves in metres/second
+	public float speed = 1f;
+
+	// distance where AppleTree turns around
+	public float leftAndRightEdge = 10f;
+
+	// chance the AppleTree will change directions
+	public float chanceToChangeDirections = 0.1f;
+
+	// rate at which Apples will be instantiated 
+	public float secondsBetweenAppleDrops = 1f;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+	//dropping apples every second	
+		InvokeRepeating ("DropApple", 2f, secondsBetweenAppleDrops);
+	}
+
+	void DropApple()
+	{
+		GameObject apple = Instantiate (applePrefab) as GameObject;
+		apple.transform.position = transform.position;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+	//basic movement
+		Vector3 pos = transform.position;
+		pos.x += speed * Time.deltaTime;
+		transform.position = pos;
+
+	//changing direction
+		if (pos.x < -leftAndRightEdge) {
+			speed = Mathf.Abs (speed); //move right
+		} else if (pos.x > leftAndRightEdge) {
+			speed = -Mathf.Abs (speed); //move left
+		} 
+	}
+
+	void FixedUpdate()
+	{
+		// changes direction randomly
+		if (Random.value < chanceToChangeDirections) 
+		{
+			speed *= -1; //change direction
+		}
 	}
 }
