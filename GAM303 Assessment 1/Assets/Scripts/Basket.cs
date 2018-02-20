222e95c2bf2f316bd29dfb2088be1ec5d75de3c1
HannibalMontannibal <33308759+HannibalMontannibal@users.noreply.github.com>
2018-02-20 12:43:22 +1100

    Text
    Hit a problem since 'GUIText' is something that's now obsolete, and am having difficulty finding an alternative.

--- GAM303 Assessment 1/Assets/Scripts/Basket.cs
+++ GAM303 Assessment 1/Assets/Scripts/Basket.cs
@@ -1,12 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Basket : MonoBehaviour {
 
+	public GUIText scoreGT;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+		// Find a reference to the ScoreCounter GameObject
+		GameObject scoreGO = GameObject.Find("ScoreCounter");
+
+		// Get the GUIText Component of that GameObject
+		scoreGT = scoreGO.GetComponent<GUIText>(); 
+
+		// Set the starting number of points to 0
+		scoreGT.text = "0";
 	}
 	
 	// Update is called once per frame
@@ -30,12 +41,21 @@ public class Basket : MonoBehaviour {
 
 	void OnCollisionEnter (Collision coll)
 	{
-	//find out what hit the basket
+		//find out what hit the basket
 		GameObject collidedWith = coll.gameObject;
 		if (collidedWith.tag == "Apple") 
 		{
 			//if what came into contact with the basket had the "apple" tag attached to it, the apple gets destroyed
 			Destroy (collidedWith);
 		}
+
+		// Parse the text of the scoreGT into an int
+		int score = int.Parse( scoreGT.text ); 
+
+		// Add points for catching the apple
+		score += 100;
+
+		// Convert the score back to a string and display it
+		scoreGT.text = score.ToString();
 	}
 }
