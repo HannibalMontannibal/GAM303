01b9cc2b4adc04a4189f461148cf9ee880f79829
HannibalMontannibal <33308759+HannibalMontannibal@users.noreply.github.com>
2018-02-28 12:30:24 +1100

    More Castles
    Made more castles.

--- GAM303 Assessment 1/Assets/Scripts/MissionDemolition/Slingshot.cs
+++ GAM303 Assessment 1/Assets/Scripts/MissionDemolition/Slingshot.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour 
 {
+	static public Slingshot S;
+
 	// fields set in the Unity Inspector pane
 	public GameObject prefabProjectile;
 	public float velocityMult = 4f;
@@ -15,29 +17,41 @@ public class Slingshot : MonoBehaviour
 	public GameObject projectile;
 	public bool aimingMode;
 
-	void Awake() {
+	void Awake() 
+	{
+		// Set the Slingshot singleton S
+		S = this;
+
 		Transform launchPointTrans = transform.Find("LaunchPoint");
+	
 		launchPoint = launchPointTrans.gameObject;
+
 		launchPoint.SetActive( false );
+
 		launchPos = launchPointTrans.position;
 	}
 
 	void Update() {
 		// If Slingshot is not in aimingMode, don't run this code
 		if (!aimingMode) return;
+
 		// Get the current mouse position in 2D screen coordinates
 		Vector3 mousePos2D = Input.mousePosition;
+
 		// Convert the mouse position to 3D world coordinates
 		mousePos2D.z = -Camera.main.transform.position.z;
 		Vector3 mousePos3D = Camera.main.ScreenToWorldPoint( mousePos2D );
+
 		// Find the delta from the launchPos to the mousePos3D
 		Vector3 mouseDelta = mousePos3D-launchPos;
+
 		// Limit mouseDelta to the radius of the Slingshot SphereCollider
 		float maxMagnitude = this.GetComponent<SphereCollider>().radius;
 		if (mouseDelta.magnitude > maxMagnitude) {
 			mouseDelta.Normalize();
 			mouseDelta *= maxMagnitude;
 		}
+
 		// Move the projectile to this new position
 		Vector3 projPos = launchPos + mouseDelta;
 		projectile.transform.position = projPos;
@@ -51,11 +65,13 @@ public class Slingshot : MonoBehaviour
 		}
 	}
 
-	void OnMouseEnter() {
+	void OnMouseEnter() 
+	{
 	//	print("Slingshot:OnMouseEnter()");
 		launchPoint.SetActive( true );
 	}
-	void OnMouseExit() {
+	void OnMouseExit() 
+	{
 	//	print("Slingshot:OnMouseExit()");
 		launchPoint.SetActive( false );
 	}
