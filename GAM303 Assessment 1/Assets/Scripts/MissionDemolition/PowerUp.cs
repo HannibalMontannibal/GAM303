79ad0b2ec66ba44fca4a534ceee781f0f00de514
Acxell Leandro <Acxell Leandro>
2018-03-08 15:31:21 +1100

    Power Up!
    Made a powerup in which the player increases in size and mass for a limited time.

---
+++ GAM303 Assessment 1/Assets/Scripts/MissionDemolition/PowerUp.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour 
+{
+	public GameObject playerModel;
+	//when the player hits the powerup, their scale increases to x3 their size for three seconds, and then sets back to normal once the three seconds is up.
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("Projectile"))
+		{
+			other.gameObject.transform.localScale = new Vector3 (3, 3, 3);
+			other.gameObject.GetComponent<Rigidbody> ().mass = 3;
+			Destroy (gameObject);
+			StartCoroutine(WaitTime(other));
+		}
+	}
+
+	IEnumerator WaitTime(Collider other)
+	{
+		yield return new WaitForSeconds (3);
+	
+		//transform.localScale += new Vector3(1, 1, 1);
+		other.gameObject.transform.localScale = new Vector3 (1, 1, 1);
+
+	}
+}
