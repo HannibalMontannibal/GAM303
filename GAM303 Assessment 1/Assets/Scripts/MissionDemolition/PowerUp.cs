10b7b77e0002e55576302d79cdd431531db47a18
Isabella <justanything1331@gmail.com>
2018-03-08 15:59:09 +1100

    Power Up Tweaking
    Trying to delete power up once it's been triggered, however that removes the object and hence the timer to turn off the power up (increase size + scale) of the player.

--- GAM303 Assessment 1/Assets/Scripts/MissionDemolition/PowerUp.cs
+++ GAM303 Assessment 1/Assets/Scripts/MissionDemolition/PowerUp.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PowerUp : MonoBehaviour 
 {
 	public GameObject playerModel;
-	//when the player hits the powerup, their scale increases to x3 their size for three seconds, and then sets back to normal once the three seconds is up.
+	//when the player hits the powerup, their scale increases to x3 their size for two seconds, and then sets back to normal once the three seconds is up.
 
 	void OnTriggerEnter(Collider other)
 	{
@@ -13,14 +13,14 @@ public class PowerUp : MonoBehaviour
 		{
 			other.gameObject.transform.localScale = new Vector3 (3, 3, 3);
 			other.gameObject.GetComponent<Rigidbody> ().mass = 3;
-			Destroy (gameObject);
 			StartCoroutine(WaitTime(other));
+			//Destroy (gameObject);
 		}
 	}
 
 	IEnumerator WaitTime(Collider other)
 	{
-		yield return new WaitForSeconds (3);
+		yield return new WaitForSeconds (2);
 	
 		//transform.localScale += new Vector3(1, 1, 1);
 		other.gameObject.transform.localScale = new Vector3 (1, 1, 1);
