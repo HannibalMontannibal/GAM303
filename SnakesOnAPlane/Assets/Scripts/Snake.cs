44210019489c066927b0f9032433ef0891ceb02a
HannibalMontannibal <33308759+HannibalMontannibal@users.noreply.github.com>
2018-04-08 16:23:29 +1000

    Base Snake Game Complete
    Just need to add the Math element to it!

---
+++ SnakesOnAPlane/Assets/Scripts/Snake.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Snake : MonoBehaviour
+{
+
+	// The current Movement Direction
+	// (as a default it moves to the right)
+	Vector2 dir = Vector2.right;
+
+	// Keeps track of the Tail
+	List<Transform> tail = new List<Transform>();
+
+	// Did the snake eat something?
+	bool snakeAte = false;
+
+	// Tail Prefab
+	public GameObject tailPrefab;
+
+	// Use this for initialization
+	void Start () 
+	{
+		// Moves the Snake every three hundred miliseconds
+		InvokeRepeating("Move", 0.3f, 0.3f);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		// For moving in a new Direction
+		if (Input.GetKey(KeyCode.D))
+			dir = Vector2.right;
+		else if (Input.GetKey(KeyCode.S))
+			dir = -Vector2.up;    // '-up' being 'down'
+		else if (Input.GetKey(KeyCode.A))
+			dir = -Vector2.right; // '-right' being 'left'
+		else if (Input.GetKey(KeyCode.W))
+			dir = Vector2.up;
+		
+	}
+
+	//handles the snake movement
+	void Move()
+	{
+		// Saves the current position 
+		Vector2 v = transform.position;
+		
+		// Moves the head in a new direction
+		transform.Translate(dir);
+
+		// Ate something? Then insert new Element into gap
+		if (snakeAte) {
+			// Load Prefab into the world
+			GameObject g =(GameObject)Instantiate(tailPrefab,
+				v,
+				Quaternion.identity);
+
+			// Keeps track of it in our tail list
+			tail.Insert(0, g.transform);
+
+			// Resets the flag
+			snakeAte = false;
+		}
+
+		// Checks whether the snake has a tail or not
+		if (tail.Count > 0)
+		{
+			// Move last Tail Element to where the Head was
+			tail.Last().position = v;
+
+			// Add to front of list, remove from the back
+			tail.Insert(0, tail.Last());
+			tail.RemoveAt(tail.Count-1);
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		// Is it Food?
+		if (coll.name.StartsWith ("FoodPrefab")) {
+			// Get longer in next Move call
+			snakeAte = true;
+
+			// Destroy the Food from the scene
+			Destroy (coll.gameObject);
+		}
+
+		// If the snake collided with either it's own Tail or Border
+		else {
+			// 'Game Over' screen
+		}
+
+	}
+}
---
+++ SnakesOnAPlane/Assets/Scripts/SpawnFood.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFood : MonoBehaviour {
+
+	//the Food prefab
+	public GameObject foodPrefab;
+
+	//the Borders
+	public Transform borderTop;
+	public Transform borderBottom;
+	public Transform borderLeft;
+	public Transform borderRight;
+
+	//spawn one piece of food
+	void Spawn()
+	{
+		// x position between left & right border
+		int x = (int)Random.Range(borderLeft.position.x,
+			borderRight.position.x);
+
+		// y position between top & bottom border
+		int y = (int)Random.Range(borderBottom.position.y, borderTop.position.y);
+
+		// Instantiate the food at (x, y) point
+		Instantiate(foodPrefab, new Vector2(x, y), Quaternion.identity); //the default rotation
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		//spawns food every five seconds, starting in three
+		InvokeRepeating("Spawn", 3, 5);
+	}
+
+}
